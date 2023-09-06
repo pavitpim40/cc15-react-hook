70532e7b910bc69103d688b4d70a05927f38e1ee
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-06 14:58:38 +0700

    css

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function App() {
+  return (
+    <div className='App'>
+      {/* Heder */}
+      <div className='header'>
+        <h1>Movie App</h1>
+      </div>
+
+      {/* Input */}
+      <div className='search'>
+        <input type='text' placeholder='keyword ?' />
+        <button>search</button>
+      </div>
+
+      {/* Result */}
+      <div className='movie-lists'>
+        <div className='movie'>Movie-1</div>
+        <div className='movie'>Movie-2</div>
+      </div>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>React Hook</h1>);
+root.render(<App />);
