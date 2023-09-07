dd6b88d3c277955eee1b75a29389f0023d371eb2
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-07 10:09:10 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -6,65 +6,59 @@ import './index.css';
 // step-2 : Make HTTP Request , after setState
 
 function App() {
-  const [category, setCategory] = useState('');
-  const [count, setCount] = useState(0);
+  const [category, setCategory] = useState('not-select');
+  const [data, setData] = useState([]);
 
-  // DEFINE
-  // url : https://jsonplaceholder.typicode.com/<categories>
+  // https://jsonplaceholder.typicode.com/posts
   async function fetchLists() {
-    const BASE_URL = 'https://jsonplaceholder.typicode.com';
     try {
-      let response = await fetch(`${BASE_URL}/${category}`, { method: 'GET' });
-      let data = await response.json();
-      console.log(data);
+      let response = await fetch(`https://jsonplaceholder.typicode.com/${category}`);
+      let dataJSON = await response.json();
+      setData(dataJSON);
     } catch (error) {
       console.log(error);
     }
   }
 
-  // Syntax : useEffect(setUp,dependenciesArray)
+  // SYNTAX : useEffect(FN,[...dependencies])
+  // When useEffect work
+  // => 1.After 1st Render
 
-  // ### 1  After  render, rerender
+  // After every Render,Rerender
   // useEffect(() => {
-  //   console.log('## 1 Effect Hook - Every render, rerender');
+  //   console.log('Effect Run');
   // });
 
-  // ### 2  After firstRender
-  useEffect(() => {
-    console.log('Only First Render');
-  }, []);
+  // After 1st Render Only
+  // useEffect(() => {
+  //   // fetchUserProfile
+  //   console.log('Effect Run');
+  // }, []);
 
-  // ### 3 : firstRender , Rerender wih category change
+  // After 1st Render , After category Change
   useEffect(() => {
-    console.log('## 2 Effect Hook - Category - MAKE HTTP with', category);
-    if (category !== '') fetchLists();
+    console.log('Effect Run :', category);
+    if (category !== 'not-select') {
+      fetchLists();
+    }
   }, [category]);
 
-  useEffect(() => {
-    console.log('## 3 : Effect Hook - Count');
-  }, [count]);
-
-  useEffect(() => {
-    console.log('## 4 : Effect Hook - All');
-  }, [count, category]);
-
-  console.log('render, rerender');
+  console.log('Render, Rerender', category);
 
   return (
     <div>
       <h1>useEffect : {category || 'not-select'}</h1>
       <div className='button__group'>
         <button onClick={() => setCategory('posts')}>posts</button>
-        <button onClick={() => setCategory('photos')}>photos</button>
-        <button onClick={() => setCategory('todos')}>todos</button>
         <button onClick={() => setCategory('users')}>users</button>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
-        <div>{count}</div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
       </div>
 
       <ul className='lists'>
-        <li className='lists__item'>item-1</li>
+        {data.map((obj) => (
+          <li className='lists__item' key={obj.id}>
+            {obj.body || obj.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
